10523f9a459c209f66326a1aaf63f228d2b12224
Bill <bill@theresnobox.net>
2012-10-26 23:13:29 -0400

    Figured, since I had a spec that will not change, I will go test first.

---
+++ test/StockController_test.rb
@@ -0,0 +1,15 @@
+require 'test/unit'
+
+#Technically a functional test, but TestUnit can work
+class StockControllerTest  < Test::Unit::TestCase
+    #Test the request of one stock
+    def testShowSingular
+
+    end
+
+    #Test the request of multiple stock
+    def testShowMultiple
+
+    end
+
+end
